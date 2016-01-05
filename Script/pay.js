f074fd2b31f35136358ca8a722f05e927d78390d
berdo272 <berdo272@yahoo.com>
2016-01-05 09:02:48 -0600

    added new functions

--- Script/pay.js
+++ Script/pay.js
@@ -7,21 +7,27 @@ hoursperweek = window.prompt ("Please enter hours per week", "37");
 payrate = window.prompt ("Please enter hourly pay rate", "22.50");
 
 function calcOvertime(hoursperweek,payrate) {
-    var overtime = (hoursperweek-40);
-    return (payrate*1.5*overtime)+(payrate*(hoursperweek-overtime));
+    return (payrate*1.5*(hoursperweek-40))+(payrate*(hoursperweek-(hoursperweek-40)));
 }
 function calcStandard(hoursperweek,payrate) {
     return (hoursperweek*payrate);
 }
-    
-
-if (hoursperweek > 40) { 
-    console.log ("hoursperweek=", hoursperweek);
+function PushOvertime(hoursperweek, payrate){
+	console.log ("hoursperweek=", hoursperweek);
     console.log ("payrate=", payrate);
     console.log ("totalpay=", Math.round (calcOvertime (hoursperweek,payrate) *100)/100);
 }
-else {
-    console.log ("hoursperweek=", hoursperweek);
+function PushStandard(hoursperweek, payrate){
+	 console.log ("hoursperweek=", hoursperweek);
     console.log ("payrate=", payrate);
     console.log ("totalpay=", Math.round (calcStandard (hoursperweek,payrate) *100)/100);
 }
+function checkIfOvertime(hoursperweek){
+if (hoursperweek > 40) { 
+PushOvertime(hoursperweek, payrate)
+}
+else {
+ PushStandard(hoursperweek, payrate)  
+}
+}
+checkIfOvertime(hoursperweek)
